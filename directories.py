55426889bcc176eb933614b9ec016509bd0a7219
mehtadevm <mehtadevm@yahoo.co.in>
2019-12-10 15:23:10 +0100

    Update directories.py
    Added the option to select which type of file to make folder.

--- directories.py
+++ directories.py
@@ -12,17 +12,31 @@ for f in files:
 extension_list = list(filter(None, extension_list))
 print('File extensions available are',set(extension_list))
 
-for i in set(extension_list):
-    folder_name = i
-    path = dir_path+'/'+folder_name
-    if(os.path.exists(path) is False):
-        os.mkdir(folder_name)
+number = 0
+avil_ext = list(set(extension_list))
+for i in avil_ext:
+    print(number,'.',i)
+    number = number + 1
+
+select = int(input("Please choose type of file."))
+print('select = ',select)
+i = avil_ext[select]
+print(i)
+#for i in set(extension_list):
+
+folder_name = i
+print('folder_name = ',folder_name)
+path = dir_path+'/'+folder_name
+if(os.path.exists(path) is False):
+    os.mkdir(folder_name)
 
 
 for f in files:
     ext = os.path.splitext(f)[1][1:].strip().lower()
-    old_path = dir_path+'/'+f
-    new_path = dir_path+'/'+ext+'/'+f
-    os.rename(old_path,new_path)
-    print('Moving to the new Path ->',new_path)
+    print(ext,'i =',i)
+    if(ext==i):
+        old_path = dir_path+'/'+f
+        new_path = dir_path+'/'+ext+'/'+f
+        os.rename(old_path,new_path)
+        print('Moving to the new Path ->',new_path)
 
